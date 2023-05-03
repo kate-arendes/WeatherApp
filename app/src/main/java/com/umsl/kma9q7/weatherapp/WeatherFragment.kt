e75e673aff6e0d27355f2dfbe8e0a95bec47903f
kma9q7 <93099088+kma9q7@users.noreply.github.com>
2023-05-03 16:22:24 -0500

    Notifications working

--- app/src/main/java/com/umsl/kma9q7/weatherapp/WeatherFragment.kt
+++ app/src/main/java/com/umsl/kma9q7/weatherapp/WeatherFragment.kt
@@ -1,8 +1,15 @@
 package com.umsl.kma9q7.weatherapp
 
+import android.Manifest
+import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.location.Address
 import android.location.Geocoder
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +19,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -23,6 +34,10 @@ class WeatherFragment : Fragment() {
 
     private var lat = 0.0
     private var long = 0.0
+    private var notificationCode = 0
+
+    val channelId = "CHANNEL 1"
+    private val channelName = "Weather Notification"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +46,8 @@ class WeatherFragment : Fragment() {
             lat = it.getDouble("lat")
             long = it.getDouble("long")
         }
+
+        createNotificationChannel()
     }
 
     override fun onCreateView(
@@ -65,10 +82,13 @@ class WeatherFragment : Fragment() {
         if(lat == 38.7092 && long == -90.3083) {
             city.text = "UMSL"
             ui.setBackgroundColor(Color.parseColor("#FFEFAFAF"))
+            notificationCode = 0
+
         }
         else {
             city.text = cityName.toString()
             ui.setBackgroundColor(Color.parseColor("#87C0F3"))
+            notificationCode = 1
         }
 
 
@@ -91,6 +111,70 @@ class WeatherFragment : Fragment() {
 
                     code.text = "OpenWeather weather code: " + weatherData.weather[0].id.toString()
 
+                    val weatherCode = weatherData.weather[0].id
+
+
+                    val notificationTitle = description.text.toString() + " in " + city.text
+                    var notificationContent = ""
+
+                    if(weatherCode < 600) {
+                        notificationContent = "It's raining outside, bring an umbrella!"
+                    }
+                    else if(weatherCode < 700) {
+                        notificationContent = "Winter weather right now. Stay warm!"
+                    }
+                    else if(weatherCode < 762)  {
+                        notificationContent = "Reduced visibility. Travel with caution."
+                    }
+                    else if(weatherCode < 800) {
+                        notificationContent = "Extreme weather. Stay alert and check local news."
+                    }
+                    else if(weatherCode == 800) {
+                        notificationContent = "It's a beautiful day in " + city.text + "!"
+                    }
+                    else {
+                        notificationContent = "Clouds right now in " + city.text
+                    }
+
+
+                    val notification = NotificationCompat.Builder(getActivity() as Context, channelId)
+                                .setContentTitle(notificationTitle)
+                                .setContentText(notificationContent)
+                                .setSmallIcon(R.drawable.ic_weather_foreground)
+                                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                .build()
+
+                    val notificationManager = this@WeatherFragment.context?.let {
+                        NotificationManagerCompat.from(
+                            it
+                        )
+                    }
+
+                    if (this@WeatherFragment.context?.let {
+                            ActivityCompat.checkSelfPermission(
+                                it,
+                                Manifest.permission.POST_NOTIFICATIONS
+                            )
+                        } != PackageManager.PERMISSION_GRANTED
+                    ) {
+
+                        ActivityCompat.requestPermissions(
+                            this@WeatherFragment.context as Activity,
+                            arrayOf(Manifest.permission.POST_NOTIFICATIONS), 0)
+
+                    } else {
+
+
+
+                        if (notificationManager != null) {
+                            if (notification != null) {
+                                notificationManager.notify(notificationCode, notification)
+                            }
+                        }
+                    }
+
+
+
                 }
             }
 
@@ -102,5 +186,14 @@ class WeatherFragment : Fragment() {
 
 
     }
+
+    fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH).apply {}
+            val manager = this@WeatherFragment.context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
 
 }
